9a0c8e69f5790b4ebd75612622f61f1ce4453d43
Spencer <spencerartisan@gmail.com>
2016-07-01 18:27:25 +0100

    Load luminaries from file

--- src/main/scala/com/artisansoftware/luminaries/Luminary.scala
+++ src/main/scala/com/artisansoftware/luminaries/Luminary.scala
@@ -1,29 +1,16 @@
 package com.artisansoftware.luminaries
 
-object Luminary {
-  val luminaries = List(
-    Luminary("Michael Crick", "MichaelLCrick"),
-    Luminary("Justin Webb", "JustinOnWeb"),
-    Luminary("Nick Robinson", "bbcnickrobinson"),
-    Luminary("Jon Snow", "jonsnowC4"),
-    Luminary("Katya Adler", "BBCkatyaadler"),
-    Luminary("David Grossman", "davidgrossman"),
-    Luminary("Laura Kuennsberg", "bbclaurak"),
-    Luminary("Eddie Mair", "eddiemair"),
-    Luminary("Sean Curran", "cripeswatson"),
-    Luminary("Kamal Ahmend", "bbckamal"),
-    Luminary("Chris Mason", "ChrisMasonBBC"),
-    Luminary("Tim Harford", "TimHarford"),
-    Luminary("Stephanie Flanders", "MyStephanomics"),
-    Luminary("Newsnight", "BBCNewsnight"),
-    Luminary("Andrew Rawnsley", "andrewrawnsley"),
-    Luminary("Steve Richards", "steverichards14"),
-    Luminary("Polly Toynbee", "pollytoynbee"),
-    Luminary("Norman Smith", "BBCNormanS"),
-    Luminary("Tim Montgomerie", "montie"),
-    Luminary("Jim Pickard", "PickardJE"),
-    Luminary("Robert Peston", "Peston"))
-}
+import java.io.{File, FileReader}
+
+import com.lambdaworks.jacks.JacksMapper
 
 case class Luminary(name: String, twitterHandle: String) {
 }
+
+object Luminary {
+  private val path: String = f"${System.getProperty("user.home")}/luminaries.json"
+  private val file = new File(path)
+  if (!file.exists()) file.createNewFile()
+  val luminaries = JacksMapper.readValue[List[Luminary]](new FileReader(path))
+}
+
