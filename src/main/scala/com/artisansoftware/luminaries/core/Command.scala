f78acf3c9706da3b37f8a81f39fe400faa2bf38e
Spencer <spencerartisan@gmail.com>
2016-07-03 10:09:28 +0100

    Move help code

--- src/main/scala/com/artisansoftware/luminaries/core/Command.scala
+++ src/main/scala/com/artisansoftware/luminaries/core/Command.scala
@@ -13,21 +13,5 @@ case class ReadTweetsCommand(hours: Int, filter: Tweet => Boolean) extends Comma
 case object ReadLuminariesCommand extends Command
 case class DeleteLuminaryCommand(twitterHandle: String) extends Command
 case class AddLuminaryCommand(twitterHandle: String, name: String) extends Command
-object Help extends SimpleCommand(List(
-  new Line(""),
-  new Line("Command Arguments:"),
-  new Line("\t[number of hours] [keywords]"),
-  new Line(""),
-  new Line("Switches:"),
-  new Line("\t[without switches]          Display recent tweets"),
-  new Line("\t-c                          Include conversation tweets (default is off)"),
-  new Line("\t-r                          Include retweets (default is off)"),
-  new Line("\t-s                          Maintain live stream of results"),
-  new Line("\t-l                          List luminaries"),
-  new Line("\t-a [twitter handle] [name]  Add a new luminary"),
-  new Line("\t-d [twitter handle]         Delete a luminary"),
-  new Line("\t-h                          Display this help"),
-  new Line("")
-))
 
 
--- src/main/scala/com/artisansoftware/luminaries/core/CommandFactory.scala
+++ src/main/scala/com/artisansoftware/luminaries/core/CommandFactory.scala
@@ -10,7 +10,7 @@ object CommandFactory {
 
   def build(commandLine: CommandLine): Command = {
     if (commandLine.hasSwitch('h'))
-      Help
+      help
     else if (commandLine.hasSwitch('l'))
       ReadLuminariesCommand
     else if (commandLine.hasSwitch('d'))
@@ -31,4 +31,21 @@ object CommandFactory {
       (t.getInReplyToUserId == -1 || commandLine.hasSwitch('c')) &&
       (commandLine.textArgs.isEmpty ||
         commandLine.textArgs.exists(keyword => t.getText.toUpperCase.contains(keyword.toUpperCase)))
+
+  val help = new SimpleCommand(List(
+    new Line(""),
+    new Line("Command Arguments:"),
+    new Line("\t[number of hours] [keywords]"),
+    new Line(""),
+    new Line("Switches:"),
+    new Line("\t[without switches]          Display recent tweets"),
+    new Line("\t-c                          Include conversation tweets (default is off)"),
+    new Line("\t-r                          Include retweets (default is off)"),
+    new Line("\t-s                          Maintain live stream of results"),
+    new Line("\t-l                          List luminaries"),
+    new Line("\t-a [twitter handle] [name]  Add a new luminary"),
+    new Line("\t-d [twitter handle]         Delete a luminary"),
+    new Line("\t-h                          Display this help"),
+    new Line("")
+  ))
 }
